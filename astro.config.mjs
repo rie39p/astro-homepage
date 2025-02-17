daadbada65abd6a4f694e5d2fc4377359398437f
koto74 <kotoganaiyo@gmail.com>
2025-02-17 19:24:39 +0900

    feat: astro-expressive-codeを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code";
+import { expressiveCodeOptions } from "./src/site.config";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [expressiveCode(expressiveCodeOptions), mdx(), sitemap()],
   vite: {
     css: {
       preprocessorOptions: {
--- src/utils/post.ts
+++ src/utils/post.ts
@@ -7,14 +7,22 @@ export const getAllPosts = async (): Promise<Array<CollectionEntry<"post">>> => 
   return posts;
 };
 
-export const getUniqueTags = async (): Promise<string[]> => {
-  const allPosts = await getCollection("post");
-  const allTags = allPosts.flatMap(post => [...post.data.tags]);
-  return [...new Set(allTags)];
+export const getAllTags = (posts: CollectionEntry<"post">[]) => {
+  return posts.flatMap(post => [...post.data.tags]);
+};
+
+export const getUniqueTags = (posts: CollectionEntry<"post">[]) => {
+  return [...new Set(getAllTags(posts))];
 };
 
 export const sortPostsByDate = (posts: Array<CollectionEntry<"post">>): Array<CollectionEntry<"post">> => {
   return posts.sort((a, b) => {
     return new Date(b.data.publishDate).getTime() - new Date(a.data.publishDate).getTime();
   });
+};
+
+export const getUniqueTagsWithCount = (posts: CollectionEntry<"post">[]): [string, number][] => {
+  return [...getAllTags(posts).reduce((acc, t) => acc.set(t, (acc.get(t) ?? 0) + 1), new Map<string, number>())].sort(
+    (a, b) => b[1] - a[1]
+  );
 };
